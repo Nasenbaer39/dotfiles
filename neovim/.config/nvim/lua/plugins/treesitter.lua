49621aab574bd211b1007c45d7aeb2ec20bfdcdd
Ben Dierks <ben.dierks@protonmail.com>
2024-07-14 19:21:30 +0200

    nvim: add lsp integration (nvim-lspconfig & none-ls)

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,14 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = function()
-	require("nvim-treesitter.install").update({ with_sync = true })()
+        require("nvim-treesitter.install").update({ with_sync = true })()
     end,
     config = function()
-	require("nvim-treesitter.configs").setup({
-	    ensure_installed = {"lua", "cpp"},
-	    sync_installed = false,
-	    highlight = { enabled = true },
-	    indent = { enabled = true },
-	})
+        require("nvim-treesitter.configs").setup({
+            auto_install = true,
+            sync_installed = false,
+            highlight = { enabled = true },
+            indent = { enabled = true },
+        })
     end,
 }
