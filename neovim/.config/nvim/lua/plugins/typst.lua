a6173ba3d0a3650523f69dab0d0b8e5ff8e10190
Ben Dierks <ben.dierks@protonmail.com>
2025-05-12 19:49:56 +0200

    add: open typst preview in new, clean browser window

--- neovim/.config/nvim/lua/plugins/typst.lua
+++ neovim/.config/nvim/lua/plugins/typst.lua
@@ -8,6 +8,39 @@ return {
         'chomosuke/typst-preview.nvim',
         lazy = false, -- or ft = 'typst'
         version = '1.*',
-        opts = {}, -- lazy.nvim will implicitly calls `setup {}`
+        opts = {
+            open_cmd = 'zen-browser --new-window %s -P Clean'
+        }, -- lazy.nvim will implicitly calls `setup {}`
+        config = function ()
+            require 'typst-preview'.setup {
+                open_cmd = 'zen-browser --new-window %s -P Clean'
+            }
+
+            require("lspconfig")["tinymist"].setup {
+                settings = {
+                    formatterMode = "typstyle",
+                    exportPdf = "onType",
+                    semanticTokens = "disable",
+                },
+
+                on_attach = function(client, bufnr)
+                    vim.keymap.set("n", "<leader>tp", function()
+                        client:exec_cmd({
+                            title = "pin",
+                            command = "tinymist.pinMain",
+                            arguments = { vim.api.nvim_buf_get_name(0) },
+                        }, { bufnr = bufnr })end, 
+                        { desc = "[T]inymist [P]in", noremap = true })
+
+                    vim.keymap.set("n", "<leader>tu", function()
+                        client:exec_cmd({
+                            title = "unpin",
+                            command = "tinymist.pinMain",
+                            arguments = { vim.v.null },
+                        }, { bufnr = bufnr }) end,
+                        { desc = "[T]inymist [U]npin", noremap = true })
+                end,
+            }
+        end
     },
 }
